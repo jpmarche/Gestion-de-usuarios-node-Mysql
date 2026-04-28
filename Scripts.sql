c8d9f03b4208984d16b2a42cf171806dcd4d93d0
jpmarche <jpmarche8@gmail.com>
2026-04-28 14:18:32 +0200

    update: corrección de scrips.sql

--- Scripts.sql
+++ Scripts.sql
@@ -6,7 +6,7 @@ USE cli_crud;
 -- 2. Crear la tabla de usuarios
 CREATE TABLE users (
     id VARCHAR(40) PRIMARY KEY,
-    username VARCHAR(40) NOT NULL,
-    email VARCHAR(40) NOT NULL UNIQUE,
-    password VARCHAR(40) NOT NULL UNIQUE
+    username VARCHAR(40) NOT NULL UNIQUE,
+    email VARCHAR(40) NOT NULL UNIQUE ,
+    password VARCHAR(40) NOT NULL 
 );
